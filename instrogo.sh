287f01e0f4b1823f759969daaec2dfbcd857c4db
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2023-04-16 21:07:45 +0800

    Update instrogo.sh

--- instrogo.sh
+++ instrogo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-domain=$(cat /root/domain)
+domain=$(cat domain)
 
 # Install Trojan Go
 latest_version="$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
